cf115ad9443253b7e65864da94af91166bc9d7be
Radai Ronald <ronald.radai@criterion.hu>
2025-01-27 11:12:36 +0100

    Adding Vestel IR protocol

--- Home_theater_Controller_software/src/IR/IRCommands.h
+++ Home_theater_Controller_software/src/IR/IRCommands.h
@@ -7,6 +7,7 @@
 uint8_t const irData[][5] = 
 {//     NAME                    ADRESS,     Mute_Command,   VolumeUp_Command,   VolumeDown_Command,     Source_Command
         /*Hitachi*/             {0x03,          0x0D,           0x10,                   0x11,               0x38},
+        /*Vestel_Tin*/          {0x01,          0x0D,           0x10,                   0x11,               0x3F},
 };
 
 #endif // IRCOMMNADS_H
